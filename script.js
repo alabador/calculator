e7fa4e6e893bc22c976ffff266e0ee63de9528ed
alabador <austinlabador@gmail.com>
2022-05-14 17:39:54 -0700

    Refactor code

--- script.js
+++ script.js
@@ -13,37 +13,6 @@ let operation = "";
 let previousOperator = '';
 let operationChosen = false;
 
-/*Basic Math Functions*/
-function add(a,b) {
-    return a + b;
-};
-
-function subtract(a,b) {
-    return a-b;
-};
-
-function multiply(a,b) {
-    return a*b;
-};
-
-function divide(a,b) {
-    return a/b;
-};
-
-function operate(operator,a,b) {
-    if (operator === '+'){
-        return add(a,b);
-    }
-    else if (operator === '-'){
-        return subtract(a,b);
-    }
-    else if (operator === 'x'){
-        return multiply(a,b);
-    }
-    else if (operator === '/'){
-        return divide(a,b);
-    }
-};
 
 /*Link buttons to display value*/
 numbers.forEach(number => {
@@ -131,63 +100,52 @@ function chooseOperation() {
 }
 
 equals.addEventListener('click', chooseOperation());
-equals.addEventListener('click', function() {
-    arrayValues.shift();
-    display.textContent = arrayValues[0];
-    displayValue = arrayValues[0];
+equals.addEventListener('click', evaluate());
+
+function evaluate() {
+    return function () {
+        arrayValues.shift();
+        display.textContent = arrayValues[0];
+        displayValue = arrayValues[0];
+        resetOperator();
+    };
+}
+
+function resetOperator() {
     operation = '';
     previousOperator = '';
     operationChosen = false;
-});
-
-// function chooseOperation() {
-//     return function() {
-//         if(arrayValues.length === 0) {
-//             arrayValues.push(0);
-//         }
-//         else if(arrayValues.length <= 2) {
-//             arrayValues.shift();
-//         }
-//         arrayValues.push(Number(displayValue));
-        
-//         if(operationChosen === false){
-//             if (operation === '-'){
-//                 arrayValues[0] = arrayValues[1]*2;
-//             }
-//             else if (operation === 'x'){
-//                 arrayValues[0] = 1;
-//             }
-//             else if (operation === '/') {
-//                 arrayValues[0] = arrayValues[1]*arrayValues[1];
-//             } 
-//         } 
-//         operationChosen = true;
-
-//         if(previousOperator === ''){
-//             arrayValues[1] = operate(operation, arrayValues[0], arrayValues[1]);
-//         }
-//         else if(previousOperator !== operation) {
-//             arrayValues[1] = operate(previousOperator, arrayValues[0], arrayValues[1]);
-//         }
-//         else {
-//             arrayValues[1] = operate(operation, arrayValues[0], arrayValues[1]);
-//         }
-//         display.textContent = arrayValues[1];
-//         displayValue = '';
-
-//     }
-// }
-
-
-// equals.addEventListener('click', function() {
-//     display.innerText = operate(operation, currentValue, Number(displayValue));
-// })
-
-
-/*Refactored Functions*/
-
-function valueReset() {
-    displayValue = '';
-    display.innerText = displayValue;
 }
+
+/*Basic Math Functions*/
+function add(a,b) {
+    return a + b;
+};
+
+function subtract(a,b) {
+    return a-b;
+};
+
+function multiply(a,b) {
+    return a*b;
+};
+
+function divide(a,b) {
+    return a/b;
+};
+
+function operate(operator,a,b) {
+    if (operator === '+'){
+        return add(a,b);
+    }
+    else if (operator === '-'){
+        return subtract(a,b);
+    }
+    else if (operator === 'x'){
+        return multiply(a,b);
+    }
+    else if (operator === '/'){
+        return divide(a,b);
+    }
+};
 
