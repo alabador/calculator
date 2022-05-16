511c834d740670decacd997bd8b7f72fee50b694
alabador <austinlabador@gmail.com>
2022-05-16 01:42:27 -0700

    Add error message when dividing by 0

--- script.js
+++ script.js
@@ -182,7 +182,12 @@ function multiply(a,b) {
 };
 
 function divide(a,b) {
-    return a/b;
+    if (b === 0) {
+        return 'ERROR';
+    }
+    else {
+        return a/b;
+    }
 };
 
 function operate(operator,a,b) {
